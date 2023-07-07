9191cc3454e27108e3049357a3acb33b349feedb
hotteok00 <112681531+hotteok00@users.noreply.github.com>
2023-07-07 14:00:48 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 1212 ms, Memory: 36312 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
@@ -9,21 +9,18 @@ public class Main {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         
         int length = 1000001;
-        int[] arr = new int[length];
+        boolean[] arr = new boolean[length];
             
         for(int i = 2; i < length; i++)
-            if(arr[i] == 0) {
-                arr[i] = i;
-                for(int j = 2 * i; j < length; j+= i) arr[j] = -1;
-            }
+            if(!arr[i]) for(int j = 2 * i; j < length; j+= i) arr[j] = true;
         
         int n; String str; boolean flag;
         while((str = reader.readLine()) != null) {
             if((n = Integer.parseInt(str)) == 0) break;
             flag = true;
             for(int i = 3; i < n; i += 2) {
-                if(arr[i] != -1 && arr[n - i] != -1) {
-                    System.out.println(n + " = " + arr[i] + " + " + arr[n - i]);
+                if(!arr[i] && !arr[n - i]) {
+                    System.out.println(n + " = " + i + " + " + (n - i));
                     flag = false;
                     break;
                 }
