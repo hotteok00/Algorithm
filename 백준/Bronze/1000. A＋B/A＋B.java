928af4b468cbbd8cd695bad7debeb95015d56794
hotteok00 <112681531+hotteok00@users.noreply.github.com>
2023-07-07 13:12:31 +0900

    [Bronze V] Title: A+B, Time: 256 ms, Memory: 18368 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+	public static void main (String[] args) {
+         Scanner a = new Scanner(System.in);
+         int i = a.nextInt();
+         int j = a.nextInt();
+         System.out.println(i+j);
+	}
+}
