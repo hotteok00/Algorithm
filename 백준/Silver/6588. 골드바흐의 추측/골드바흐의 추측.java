b87f6a0b5f2f223575ee2d54fdfc1d3526c0bae0
hotteok00 <112681531+hotteok00@users.noreply.github.com>
2023-07-07 13:58:38 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 1348 ms, Memory: 40276 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
@@ -1,8 +1,12 @@
-import java.util.Scanner;
+// import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		// Scanner sc = new Scanner(System.in);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         
         int length = 1000001;
         int[] arr = new int[length];
@@ -13,9 +17,9 @@ public class Main {
                 for(int j = 2 * i; j < length; j+= i) arr[j] = -1;
             }
         
-        int n; boolean flag;
-        while(true) {
-            if((n = Integer.parseInt(sc.next())) == 0) break;
+        int n; String str; boolean flag;
+        while((str = reader.readLine()) != null) {
+            if((n = Integer.parseInt(str)) == 0) break;
             flag = true;
             for(int i = 3; i < n; i += 2) {
                 if(arr[i] != -1 && arr[n - i] != -1) {
@@ -27,6 +31,6 @@ public class Main {
             if(flag) System.out.println("Goldbach's conjecture is wrong.");
         }
         
-        sc.close();
+        // sc.close();
 	}
 }
