71619d2732372ecc0f1b05318aa02ccde8dd8222
hotteok00 <112681531+hotteok00@users.noreply.github.com>
2024-07-02 16:48:12 +0900

    [Platinum V] Title: 버블 소트, Time: 2892 ms, Memory: 89984 KB -BaekjoonHub

---
+++ 백준/Platinum/1517. 버블 소트/버블 소트.py
@@ -0,0 +1,48 @@
+from sys import stdin, stdout
+input = stdin.readline
+output = stdout.write
+
+result = 0
+
+N = int(input())
+arr = list(map(int, input().split()))
+
+def merge_sort(s, e):
+    global result
+
+    if e - s < 1: return
+
+    m = (s + e) // 2
+    merge_sort(s, m)
+    merge_sort(m + 1, e)
+
+    tmp = []
+    for i in range(s, e + 1):
+        tmp.append(arr[i])
+
+    flag = s
+    idxR = s
+    idxL = m + 1
+    while idxR <= m and idxL <= e:
+        if tmp[idxR - s] < tmp[idxL - s]: 
+            arr[flag] = tmp[idxR - s]
+            idxR += 1
+        elif tmp[idxR - s] > tmp[idxL - s]:
+            arr[flag] = tmp[idxL - s]
+            result = result + idxL - flag
+            idxL += 1
+        else:
+            idxR += 1
+        flag += 1
+    while idxR <= m:
+        arr[flag] = tmp[idxR - s]
+        idxR += 1
+        flag += 1
+    while idxL <= e:
+        arr[flag] = tmp[idxL - s]
+        idxL += 1
+        flag += 1
+
+merge_sort(0, N-1)
+
+output(str(result))
