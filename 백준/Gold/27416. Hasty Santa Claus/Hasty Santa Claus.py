0044107e8d36133771c3ec8d8464c0f0cfaf042d
hotteok00 <112681531+hotteok00@users.noreply.github.com>
2024-07-27 17:55:00 +0900

    [Gold IV] Title: Hasty Santa Claus, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/27416. Hasty Santa Claus/Hasty Santa Claus.py
+++ 백준/Gold/27416. Hasty Santa Claus/Hasty Santa Claus.py
@@ -1,45 +1,23 @@
 from sys import stdin, stdout
-from collections import defaultdict
-
 input = stdin.readline
-print = stdout.write
+output = stdout.write
 
 n, k = map(int, input().split())
-houses = []
+vacations = []
 for i in range(n):
     s, e = map(int, input().split())
-    houses.append((s, e, i))
+    vacations.append([i, s, e])
+    
+vacations.sort(key=lambda x: (x[2],x[1]))
 
-# Create a list of available houses for each day
-available = defaultdict(list)
-for s, e, i in houses:
+visit_schedule = [0] * n
+day_count = [0] * 32
+for idx, s, e in vacations:
     for day in range(s, e + 1):
-        available[day].append((e, i))  # Store end day and house index
-
-visited = [False] * n
-schedule = [0] * n
-
-# Schedule visits
-for day in range(1, 32):
-    if not available[day]:
-        continue
-    
-    # Sort houses available today by end date
-    available[day].sort()
-    
-    visits = 0
-    for _, i in available[day]:
-        if not visited[i] and visits < k:
-            schedule[i] = day
-            visited[i] = True
-            visits += 1
-        
-        if visits == k:
+        if day_count[day] < k:
+            day_count[day] += 1
+            visit_schedule[idx] = day
             break
 
-# Check if all houses were visited
-if all(visited):
-    for day in schedule:
-        print(f"{day}\n")
-else:
-    print("No solution exists\n")
+for v in visit_schedule:
+    output(str(v) + '\n')
