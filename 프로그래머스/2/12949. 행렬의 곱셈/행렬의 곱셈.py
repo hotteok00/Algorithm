4ae4a95244934901c956ac7d54c08eec6faa03cf
hotteok00 <112681531+hotteok00@users.noreply.github.com>
2024-09-25 12:54:28 +0900

    [level 2] Title: 행렬의 곱셈, Time: 110.00 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -2,16 +2,26 @@ def solution(arr1, arr2):
     answer = []
     
     # [a by b] * [b by c]
-    for idx_a, a in enumerate(arr1):
-        answer.append([])
-        for idx_c in range(len(arr2[0])):
-            b = [ arr2[idx_b][idx_c] for idx_b in range(len(a)) ]
+#     for idx_a, a in enumerate(arr1):
+#         answer.append([])
+#         for idx_c in range(len(arr2[0])):
+#             b = [ arr2[idx_b][idx_c] for idx_b in range(len(a)) ]
             
-            sumOfMul_arr1_arr2 = 0
-            for idx in range(len(a)):
-                sumOfMul_arr1_arr2 += a[idx] * b[idx]
+#             sumOfMul_arr1_arr2 = 0
+#             for idx in range(len(a)):
+#                 sumOfMul_arr1_arr2 += a[idx] * b[idx]
             
-            answer[idx_a].append(sumOfMul_arr1_arr2)
-            
+#             answer[idx_a].append(sumOfMul_arr1_arr2)
+    
+    len_a = len(arr1)
+    len_b = len(arr2)
+    len_c = len(arr2[0])
+    
+    answer = [[0 for _ in range(len_c)] for _ in range(len_a)]
+    
+    for i in range(len_a):
+        for j in range(len_c):
+            for k in range(len_b):
+                answer[i][j] += arr1[i][k] * arr2[k][j]
     
     return answer
