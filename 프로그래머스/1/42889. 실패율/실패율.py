240322f548827b1299023c468577c82c7da61704
hotteok00 <112681531+hotteok00@users.noreply.github.com>
2024-09-26 10:32:13 +0900

    [level 1] Title: 실패율, Time: 1579.15 ms, Memory: 15 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,18 +1,30 @@
 def solution(N, stages):
     answer = []
     
-    failureRate = [[0, 0, idx] for idx in range(N + 2)]    # not clear yet, arrived
-    for s in stages:
-        failureRate[s][0] += 1
-        for i in range(1, s + 1): failureRate[i][1] += 1
+#     failureRate = [[0, 0, idx] for idx in range(N + 2)]    # not clear yet, arrived
+#     for s in stages:
+#         failureRate[s][0] += 1
+#         for i in range(1, s + 1): failureRate[i][1] += 1
     
-    for idx in range(N + 2): 
-        b = failureRate[idx][1]
-        if b == 0: failureRate[idx][1] = 1
+#     for idx in range(N + 2): 
+#         b = failureRate[idx][1]
+#         if b == 0: failureRate[idx][1] = 1
     
-    failureRate = failureRate[1:-1]
-    failureRate.sort(key = lambda x: -x[0]/x[1])
+#     failureRate = failureRate[1:-1]
+#     failureRate.sort(key = lambda x: -x[0]/x[1])
     
-    for a, b, idx in failureRate: answer.append(idx)
+#     for a, b, idx in failureRate: answer.append(idx)
+
+    fails = {}
+    total = len(stages)
+    for stage in range(1, N + 1):
+        if total != 0:
+            cnt = stages.count(stage)
+            fails[stage] = cnt / total
+            total -= cnt
+        else:
+            fails[stage] = 0
+    
+    answer = sorted(fails, key=lambda x: fails[x], reverse=True)
     
     return answer
