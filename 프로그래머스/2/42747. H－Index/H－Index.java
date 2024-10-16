07dce648f3f610255552f8a31da65a0aa296e42f
hotteok00 <112681531+hotteok00@users.noreply.github.com>
2024-10-17 00:17:23 +0900

    [level 2] Title: H-Index, Time: 1.06 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+        int n = citations.length;
+        int h = 0;
+        
+        for (int i = 0; i < n; i++) {
+            int smaller = Math.min(citations[i], n - i);
+            h = Math.max(h, smaller);
+        }
+        
+        return h;
+    }
+}
