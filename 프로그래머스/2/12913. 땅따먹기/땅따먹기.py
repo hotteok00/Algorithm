1b9bf2bddd33486dadab942fea155ba8262a9098
hotteok00 <112681531+hotteok00@users.noreply.github.com>
2025-01-10 11:18:10 +0900

    [level 2] Title: 땅따먹기, Time: 360.99 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -0,0 +1,8 @@
+def solution(land):
+    n = len(land)
+    
+    for i in range(1, n):
+        for j in range(4):
+            land[i][j] += max([land[i-1][k] for k in range(4) if k != j])
+    
+    return max(land[-1])
